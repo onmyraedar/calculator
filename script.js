5e74109943278ce684ec34b7c4d4b124a808ce2d
Reanna Ishmael <rishmael@fordham.edu>
2022-06-22 00:40:03 -0400

    Add operate function
    operate() executes the correct math operation based on the button the user has pressed.

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(a, b, operator) {
+    if (operator === "plusBtn") {
+        return add(a, b);
+    } else if (operator === "minusBtn") {
+        return subtract(a, b);
+    } else if (operator === "multiplyBtn") {
+        return multiply(a, b);
+    } else if (operator === "divideBtn") {
+        return divide(a, b);
+    } else {
+        return "Error";
+    }
+}
